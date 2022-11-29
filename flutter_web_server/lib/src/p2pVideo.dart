78f8b64259e387e23c9a02dc22f0ed151a041637
Chris Apaza <chrisalanapazaa@gmail.com>
2022-11-29 18:28:49 -0400

    craeted flutter project

---
+++ flutter_web_server/lib/src/p2pVideo.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class P2PVideo extends StatefulWidget {
+  const P2PVideo({super.key});
+
+  @override
+  State<P2PVideo> createState() => _P2PVideoState();
+}
+
+class _P2PVideoState extends State<P2PVideo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
